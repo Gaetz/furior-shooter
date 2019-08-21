e73ec80fa14317a667b726f7ec256ac0cfceff45
Gaetan Blaise-Cazalet <gblaisec@artfx.fr>
2019-08-21 17:14:47 +0200

    Start game

---
+++ FuriorShooter/Intermediate/Build/Win64/FuriorShooterEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/ArtFx/Dev/furior-shooter/FuriorShooter/Intermediate/Build/Win64/FuriorShooterEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ FuriorShooter/Source/FuriorShooter/FuriorShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FuriorShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FuriorShooter, "FuriorShooter" );
